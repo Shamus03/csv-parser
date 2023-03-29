186c0bb5041261d71084360a60e817d716ffd976
Shamus Taylor <jtaylor@labattfood.com>
2023-03-29 10:23:42 -0500

    fix: update types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 declare module '@shamus03/csv-parser' {
   export interface Options {
     separator?: string
+    allowExtraContentAfterQuotedValue?: boolean
   }
   function parse(str: string): string[][];
   function parse(str: string, opts: Options): string[][];
